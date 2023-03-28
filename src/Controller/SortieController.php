5af075c71a0e2079f519e5a589026f30922b01c2
gnicolas2022 <gwenaelle_nicolas@yahoo.fr>
2023-03-28 10:28:26 +0200

    Maj Gwen Creation Sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,17 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/sorties', name: 'sortie_')]
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function index(): Response
+    #[Route('', name: 'accueil')]
+    public function list(SortieRepository $sortieRepository): Response
     {
-        return $this->render('sortie/index.html.twig', [
+        return $this->render('sortie/accueil.html.twig', [
             'controller_name' => 'SortieController',
         ]);
     }
+
+
+    #[Route('/detail/{id}', name: 'detail')]
+    public function detail(int $id, SortieRepository $sortieRepository): Response
+    {
+        $sortie = $sortieRepository->find($id);
+
+        return $this->render('sortie/detail.html.twig', [
+            'sortie' => $sortie
+        ]);
+    }
+
+
+    #[Route('/creer', name: 'creer')]
+    public function creer(Request $request, EntityManagerInterface $entityManager): Response
+    {
+       $sortie = new Sortie();
+
+       $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        dump($sortie);
+        $sortieForm->handleRequest($request);
+        dump($sortie);
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre sortie a été ajoutée !');
+            return $this->redirectToRoute( 'sortie_detail',
+            ['id'=>$sortie->getId()]);
+        }
+
+        dump($request);
+        return $this->render('sortie/creer.html.twig', [
+            'sortieForm' => $sortieForm->createView()
+        ]);
+    }
+
+
 }
---
+++ src/Form/SortieType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('dateHeureDebut', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('duree')
+            ->add('dateLimiteInscription', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('nbInscriptionMax')
+            ->add('infosSortie')
+            ->add('etat', ChoiceType::class, [
+                'choices' => [
+                    'Créée' => 'Créée',
+                    'Ouverte' => 'Ouverte',
+                    'Clôturée' => 'Clôturée',
+                    'Activité en cours' => 'Activité en cours',
+                    'Passée' => 'Passée',
+                    'Annulée' => 'Annulée'
+                ],
+                'multiple' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
