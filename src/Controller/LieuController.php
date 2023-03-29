73547c3fb27fdf0cf2a1df0373a1356f2da54829
gnicolas2022 <gwenaelle_nicolas@yahoo.fr>
2023-03-29 16:51:25 +0200

    MAJ de Gwen avec Json et tutti quanti ajax

---
+++ src/Controller/LieuController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LieuRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+$encoders = [new XmlEncoder(), new JsonEncoder()];
+$normalizers = [new ObjectNormalizer()];
+
+$serializer = new Serializer($normalizers, $encoders);
+
+class LieuController extends AbstractController
+{
+
+    #[Route('/lieu/{id}', name: 'lieu')]
+    public function affichageLieu(int $id, LieuRepository $lieuRepository): JsonResponse
+    {
+
+        $lieu = $lieuRepository->find($id);
+
+        if (!$lieu) {
+            throw $this->createNotFoundException('Lieu non trouvé');
+        }
+        dump($lieu);
+
+            return $this->json([
+                'nom' => $lieu->getNom(),
+                'rue' => $lieu->getRue(),
+                'latitude' => $lieu->getLatitude(),
+                'longitude' => $lieu->getLongitude(),
+                'ville' => [
+                    'nom' => $lieu->getVille()->getNom(),
+                    'code_postal' => $lieu->getVille()->getCodePostal(),
+                ],
+            ]);
+
+        //return $this->json($jsonContent);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -76,7 +76,7 @@ class SortieController extends AbstractController
     #[Route('/creer', name: 'creer')]
     public function creer(Request $request, EntityManagerInterface $entityManager, LieuRepository $lieuRepository, VilleRepository $villeRepository): Response
     {
-        $listeLieu = $lieuRepository->findAll();
+        $lieu = $lieuRepository->findAll();
         $Ville = $villeRepository->findAll();
 
 
@@ -101,7 +101,7 @@ class SortieController extends AbstractController
         dump($request);
         return $this->render('sortie/creer.html.twig', [
             'sortieForm' => $sortieForm->createView(),
-            'listeLieu' => $listeLieu,
+            'lieu' => $lieu,
             'Ville' => $Ville
         ]);
     }
