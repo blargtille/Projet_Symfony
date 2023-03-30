9cdb0e277a6711f0dff8c51e9531ea9ee4cffed2
gnicolas2022 <gwenaelle_nicolas@yahoo.fr>
2023-03-30 11:47:46 +0200

    le pavé créer sortie fini manque plus que les fonctions des boutons

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\LieuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
